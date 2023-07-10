c8a9d0dfe59c72e25d639c34ca53e11f29a2ac98
NehaParakh1 <nehadugar3@gmail.com>
2023-07-10 13:44:57 +0530

    final changes

--- src/Component/Product/AvailableProduct.js
+++ src/Component/Product/AvailableProduct.js
@@ -10,25 +10,25 @@ import ProductItem from './ProductItem';
           id:'a1',
         title: 'Colors',
         price: 100,
-        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+        image: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
         },
         {
           id:'a2',
         title: 'Black and white Colors',
         price: 50,
-        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+        image: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
         },
         {
           id:'a3',
         title: 'Yellow and Black Colors',
         price: 70,
-        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+        image: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
         }, 
         {
           id:'a4',
         title: 'Blue Color',
         price: 100,
-        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+        image: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
         }
         ]
 
@@ -40,7 +40,7 @@ import ProductItem from './ProductItem';
       id={item.id}
       title={item.title}
       price={item.price}
-      image={item.imageUrl}
+      image={item.image}
     />
   ));
   return (
